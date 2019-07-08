66a98d16968fbc915cfe45c603886a84b9258e1e
eunsolum <eunsolum@naver.com>
2019-07-08 17:25:11 +0900

    exercise

---
+++ 002.py
@@ -0,0 +1,8 @@
+r=3
+PI = 3.14
+
+surface_area = r*r*PI
+
+print(surface_area)
+
+'''import math'''
---
+++ 004.py
@@ -0,0 +1,7 @@
+
+num = 3
+
+if num % 2 == 0:
+    print(num1,'is even')
+else:
+    print(num1,'is odd')
